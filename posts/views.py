d7f1ed24002175b1786ebfc23d3fb994ed2b6776
arafatsheikh098 <arafat.csecu@gmail.com>
2025-02-13 17:43:35 +0600

    feaching data from post table and show in all post page

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from posts.models import Post
 # Create your views here.
+def post_list_view(request):
+    post_data = Post.objects.all().order_by("id")
+    context = {
+        "data":post_data
+    }
+
+    return render(request,"post_list.html",context)
