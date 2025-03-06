a6d272ffbe0b528c0352a96ff533a2528a9e9573
arafatsheikh098 <arafat.csecu@gmail.com>
2025-03-06 16:18:20 +0600

    Signup and Login page updated

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import post_list_view,portfolio_view,single_post_view
+from posts.views import post_list_view,portfolio_view,single_post_view,signup,login
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post-list/',post_list_view),
     path('portfolio/',portfolio_view),
     path('single-post/<int:id>',single_post_view,name='one-page'),
+    path('signup/',signup,name='signup'),
+    path('login/',login,name='login'),
+
 
 ]
 from django.conf import settings
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from posts.models import Post,comment
 from django.views.generic import ListView
 # Create your views here.
@@ -21,10 +21,19 @@ def portfolio_view(request):
     }
     return render(request,"portfolio.html",context)
 def single_post_view(request,id):
-    single_post_data=Post.objects.get(id=id)
+    # single_post_data=Post.objects.get(id=id)
+    single_post_data=get_object_or_404(Post,id=id)
+    # try:
+    #     single_post_data=Post.objects.get(id=id)
+    # except obje
+
     single_post_comment=comment.objects.filter(post_id=id).select_related("user")
     context={
         "post":single_post_data,
         "comment":single_post_comment
     }
     return render(request,"single_post.html",context)
+def signup(request):
+    return render(request, 'signup.html')
+def login(request):
+    return render(request, 'login.html')
