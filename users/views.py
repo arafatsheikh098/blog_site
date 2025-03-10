d9857534ddf368e34bd01e20900299e51fbf27e1
arafatsheikh098 <arafat.csecu@gmail.com>
2025-03-10 21:15:42 +0600

    Signup data post in database

--- users/views.py
+++ users/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == "POST":
+        
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        confirmpassword=request.POST.get("cPassword")
+        User.objects.create_user(username=username,email=email,password=password)
+        return render(request,"login.html")
+    else :
+      return render(request, 'signup.html')
 def login(request):
     return render(request, 'login.html')
